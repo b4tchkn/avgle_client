96cf62c4495dc7b7371c5366e650d393a23e831d
batch <baaaakkrad@gmail.com>
2020-08-24 01:57:30 +0900

    add Lottie

--- lib/ui/library/library_page.dart
+++ lib/ui/library/library_page.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:lottie/lottie.dart';
 
 class LibraryPage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('This is LibraryPage. Coming soon.'),
+        child: Lottie.network(
+            'https://assets7.lottiefiles.com/packages/lf20_C31OHO.json'),
       ),
     );
   }
