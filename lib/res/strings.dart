50d2af6a747fd84121f003a8a0f88e30bb3d66d7
batch <baaaakkrad@gmail.com>
2020-08-27 21:36:28 +0900

    add ModalBottomSheet in Home

--- lib/res/strings.dart
+++ lib/res/strings.dart
@@ -2,4 +2,7 @@ class Strings {
   static const bottomNavItemHomeTitle = 'ホーム';
   static const bottomNavItemExploreTitle = '探索';
   static const bottomNavItemLibraryTitle = 'ライブラリ';
+
+  static const homeModalBottomSheetSaveWatchLater = '後で見るに保存';
+  static const homeModalBottomSheetSavePlayList = '再生リストに保存';
 }
--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:avgleclient/app_notifier.dart';
 import 'package:avgleclient/data/model/video_res.dart';
 import 'package:avgleclient/error_notifier.dart';
+import 'package:avgleclient/res/app_colors.dart';
+import 'package:avgleclient/res/strings.dart';
 import 'package:avgleclient/ui/home/home_view_model.dart';
 import 'package:avgleclient/util/converters.dart';
 import 'package:flutter/cupertino.dart';
@@ -36,7 +38,7 @@ class HomePage extends HookWidget {
                     controller: scrollController,
                     itemCount: viewModel.videos.length,
                     itemBuilder: (BuildContext context, int index) {
-                      return _videoItem(viewModel.videos[index]);
+                      return _videoItem(viewModel.videos[index], context);
                     }),
               )
             : const CircularProgressIndicator(),
@@ -44,7 +46,7 @@ class HomePage extends HookWidget {
     );
   }
 
-  Widget _videoItem(Video video) {
+  Widget _videoItem(Video video, BuildContext context) {
     final viewCount = Converters.toViewCountFormatted(video.viewnumber);
     final addedAt = Converters.toAddedAtFormatted(video.addtime);
 
@@ -135,7 +137,12 @@ class HomePage extends HookWidget {
                         size: 20,
                       ),
                       onPressed: () {
-                        debugPrint('more tapped');
+                        _showModalBottomSheet(context, video);
+//                        Scaffold.of(context).showSnackBar(
+//                          const SnackBar(
+//                            content: Text('Tapped more'),
+//                          ),
+//                        );
                       },
                     ),
                   ),
@@ -146,5 +153,73 @@ class HomePage extends HookWidget {
         ),
       ),
     );
+  }
+
+  void _showModalBottomSheet(BuildContext context, Video video) {
+    showModalBottomSheet<String>(
+        context: context,
+        builder: (BuildContext bc) {
+          return Wrap(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(8),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image(
+                      image: NetworkImage(video.previewUrl),
+                      height: 50,
+                      width: 80,
+                      fit: BoxFit.cover,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(8),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            video.title,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.all(4),
+                          ),
+                          Text(
+                            '${video.duration}',
+                            maxLines: 1,
+                            overflow: TextOverflow.clip,
+                            style: const TextStyle(
+                              color: AppColors.lightGrey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const Divider(),
+              ListTile(
+                leading: const Icon(Icons.music_note),
+                title: const Text(Strings.homeModalBottomSheetSaveWatchLater),
+                onTap: () {
+                  // TODO 後で見るに保存する処理
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.ac_unit),
+                title: const Text(Strings.homeModalBottomSheetSavePlayList),
+                onTap: () {
+                  // TODO プレイリストに保存するためのダイアログ表示
+                },
+              )
+            ],
+          );
+        });
   }
 }
