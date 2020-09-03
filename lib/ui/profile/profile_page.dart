c2ad8eb614c2fa15a3cc96dbd4d8c6c2156894cd
batch <baaaakkrad@gmail.com>
2020-09-03 12:06:33 +0900

    add login info in ProfilePage

--- lib/ui/profile/profile_page.dart
+++ lib/ui/profile/profile_page.dart
@@ -1,6 +1,7 @@
 import 'package:avgleclient/res/app_colors.dart';
 import 'package:avgleclient/res/strings.dart';
 import 'package:avgleclient/ui/debug/debug_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -8,6 +9,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 class ProfilePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
+    final auth = FirebaseAuth.instance;
     return Scaffold(
       appBar: AppBar(
         title: const Text(Strings.profileAppBarTitle),
@@ -17,6 +19,10 @@ class ProfilePage extends HookWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
+              if (auth.currentUser != null)
+                Text(auth.currentUser.toString())
+              else
+                const Text('ログインしてね'),
               const Divider(),
               FlatButton(
                 child: const Text(Strings.debugPageTitle),
