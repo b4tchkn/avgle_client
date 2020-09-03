8954119e67d1bfed1074a2d710298ca0acc8383e
batch <baaaakkrad@gmail.com>
2020-09-03 11:29:41 +0900

    add Google Signin Function

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:avgleclient/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() => runApp(ProviderScope(child: App()));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(ProviderScope(child: App()));
+}
--- lib/ui/profile/profile_page.dart
+++ lib/ui/profile/profile_page.dart
@@ -1,10 +1,16 @@
 import 'package:avgleclient/res/app_colors.dart';
 import 'package:avgleclient/res/strings.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ProfilePage extends HookWidget {
+  final auth = FirebaseAuth.instance;
+  final googleSignIn = GoogleSignIn();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,12 +25,33 @@ class ProfilePage extends HookWidget {
               FlatButton(
                 child: Text('Google'),
                 color: AppColors.lightGrey,
-                onPressed: () {},
+                onPressed: () {
+                  _signIn().then((value) => debugPrint(value.displayName));
+                },
               )
             ],
           ),
         ),
       ),
     );
+  }
+
+  Future<User> _signIn() async {
+    await Firebase.initializeApp();
+    var currentUser = googleSignIn.currentUser;
+    try {
+      currentUser ??= await googleSignIn.signIn();
+
+      final googleAuth = await currentUser.authentication;
+      final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+      final user = (await auth.signInWithCredential(credential)).user;
+      debugPrint('signed in $user');
+
+      return user;
+    } catch (e) {
+      debugPrint(e.toString());
+      return null;
+    }
   }
 }
