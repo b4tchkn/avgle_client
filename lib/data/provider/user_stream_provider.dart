bfa9530569ed9a4d804a612fa3d3b72471e0721e
batch <baaaakkrad@gmail.com>
2020-09-06 17:09:18 +0900

    fix stream bug error

--- lib/data/provider/user_stream_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-final Provider<Stream<User>> userStreamProvider =
-    Provider<Stream<User>>((_) => FirebaseAuth.instance.authStateChanges());
--- lib/ui/library/library_page.dart
+++ lib/ui/library/library_page.dart
@@ -1,18 +1,16 @@
-import 'package:avgleclient/data/provider/user_stream_provider.dart';
 import 'package:avgleclient/ui/library/widgets/library_page_singed_in.dart';
 import 'package:avgleclient/ui/library/widgets/library_page_singned_out.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class LibraryPage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User>(
-      stream: useProvider(userStreamProvider),
-      builder: (context, snapshot) {
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (_, snapshot) {
         if (snapshot.connectionState == ConnectionState.active) {
           final user = snapshot.data;
           if (user == null) {
@@ -23,7 +21,9 @@ class LibraryPage extends HookWidget {
         } else {
           return const Scaffold(
             body: Center(
-              child: CircularProgressIndicator(),
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.red,
+              ),
             ),
           );
         }
