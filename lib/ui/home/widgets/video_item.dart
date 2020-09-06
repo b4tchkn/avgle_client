4c004f9ef4db467c1b20ddaa3c1e1b5bc73525ee
batch <baaaakkrad@gmail.com>
2020-09-06 17:12:06 +0900

    fix VideoMoreModaiBottomSheet name

--- lib/ui/home/widgets/video_item.dart
+++ lib/ui/home/widgets/video_item.dart
@@ -1,7 +1,7 @@
 import 'package:avgleclient/data/model/video_res.dart';
 import 'package:avgleclient/ui/core/video_web_view.dart';
 import 'package:avgleclient/ui/home/home_view_model.dart';
-import 'package:avgleclient/ui/home/widgets/VideoMoreModalBottomSheet.dart';
+import 'package:avgleclient/ui/home/widgets/video_more_modal_bottom_sheet.dart';
 import 'package:avgleclient/util/converters.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
