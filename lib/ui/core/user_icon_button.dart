47cf8ff59120c524568f2281adcf988a39d53cea
batch <baaaakkrad@gmail.com>
2020-09-15 11:28:31 +0900

    fix user icon in Appbar

--- lib/ui/core/user_icon_button.dart
+++ lib/ui/core/user_icon_button.dart
@@ -1,38 +1,68 @@
-import 'package:avgleclient/data/provider/firebase_auth_provider.dart';
 import 'package:avgleclient/ui/profile/profile_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class UserIconButton extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    final auth = useProvider(firebaseAuthProvider);
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Material(
-        elevation: 4.0,
-        shape: const CircleBorder(),
-        clipBehavior: Clip.hardEdge,
-        color: Colors.transparent,
-        child: Ink.image(
-          image: auth.currentUser == null
-              ? const NetworkImage(
+    return StreamBuilder<User>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (_, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final user = snapshot.data;
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Material(
+              elevation: 4.0,
+              shape: const CircleBorder(),
+              clipBehavior: Clip.hardEdge,
+              color: Colors.transparent,
+              child: Ink.image(
+                image: user == null
+                    ? const NetworkImage(
+                        'https://avgle.com/media/users/nopic-Male.gif',
+                      )
+                    : NetworkImage(user.photoURL),
+                fit: BoxFit.cover,
+                width: 40.0,
+                height: 40.0,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push<Widget>(context,
+                        MaterialPageRoute(builder: (_) => ProfilePage()));
+                  },
+                ),
+              ),
+            ),
+          );
+        } else {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Material(
+              elevation: 4.0,
+              shape: const CircleBorder(),
+              clipBehavior: Clip.hardEdge,
+              color: Colors.transparent,
+              child: Ink.image(
+                image: const NetworkImage(
                   'https://avgle.com/media/users/nopic-Male.gif',
-                )
-              : NetworkImage(auth.currentUser.photoURL),
-          fit: BoxFit.cover,
-          width: 40.0,
-          height: 40.0,
-          child: InkWell(
-            onTap: () {
-              Navigator.push<Widget>(
-                  context, MaterialPageRoute(builder: (_) => ProfilePage()));
-            },
-          ),
-        ),
-      ),
+                ),
+                fit: BoxFit.cover,
+                width: 40.0,
+                height: 40.0,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push<Widget>(context,
+                        MaterialPageRoute(builder: (_) => ProfilePage()));
+                  },
+                ),
+              ),
+            ),
+          );
+        }
+      },
     );
   }
 }
